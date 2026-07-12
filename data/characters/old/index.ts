5ee7913ad3123a2908ce671cd43148c5e08bc905
soyleyiciugur <soyleyiciugur@gmail.com>
2026-07-12 19:35:56 +0300

    Added all Admin Panel sections. Added The Raven's Eye.

--- data/characters/old/index.ts
+++ data/characters/old/index.ts
@@ -1,7 +1,7 @@
 // This file is C:\Users\Locpick-13\a-song-of-fire-and-blood\data\characters\old\index.ts
 import { z } from "zod";
-import charactersData from "./characters.json";
-import { CharacterSchema, type CharacterFromSchema } from "../../schemas/character"; 
+import charactersData from "../characters.json";
+import { CharacterSchema, type CharacterFromSchema } from "../../../schemas/character"; 
 // (Eğer schemas klasörünün yolu farklıysa "../../schemas/character" kısmını doğru yola göre düzeltmelisin)
 
 // JSON'ı senin karakter kurallarına göre test edip içeri alıyoruz
