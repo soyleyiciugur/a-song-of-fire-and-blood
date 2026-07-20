a587e79845343a75645dc3f00e994f7105df5586
soyleyiciugur <soyleyiciugur@gmail.com>
2026-07-20 14:53:09 +0300

    Minor updates.

--- lib/chapters.ts
+++ lib/chapters.ts
@@ -1,10 +1,6 @@
 // This file is C:\Users\Locpick-13\a-song-of-fire-and-blood\lib\chapters.ts
-import { chapters } from "@/data/chapters";
+import { getAllChapters, getChapter } from "@/data/chapters";
+export type { Chapter } from "@/data/chapters";
 
-export function getChapters() {
-  return Object.values(chapters);
-}
-
-export function getChapter(slug: string) {
-  return chapters[slug];
-}
+export const getChapters = getAllChapters;
+export { getChapter };
